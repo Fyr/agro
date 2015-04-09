b6ff7cdd4ada5a37c58740b2ef8e3c6982fbf7bd
Fyr <fyr.work@gmail.com>
2015-04-09 15:14:22 +0200

    Подправил медиа-роутер

--- app/plugins/media/controllers/router_controller.php
+++ app/plugins/media/controllers/router_controller.php
@@ -8,10 +8,13 @@ class RouterController extends MediaAppController {
 		App::import('Helper', 'media.PHMedia');
 		$this->PHMedia = new PHMediaHelper();
 
-		list($domain, $zone) = explode('.', DOMAIN_NAME);
-		$fname = $this->PHMedia->getFileName($type, $id, $size, $zone.'_'.$filename);
 		$aFName = $this->PHMedia->getFileInfo($filename);
-
+		$fname = $this->PHMedia->getFileName($type, $id, $size, $filename);
+		if ($type == 'product') {
+			list($domain, $zone) = explode('.', DOMAIN_NAME);
+			$fname = str_replace('.'.$aFName['ext'], '_'.$zone.'.'.$aFName['ext'], $fname);
+		}
+		
 		if (file_exists($fname)) {
 			header('Content-type: image/'.$aFName['ext']);
 			echo file_get_contents($fname);
@@ -22,7 +25,8 @@ class RouterController extends MediaAppController {
 
 		App::import('Vendor', 'Image', array('file' => '../plugins/media/vendors/image.class.php'));
 		$image = new Image();
-		$image->load($this->PHMedia->getFileName($type, $id, null, $aFName['orig_fname'].'.'.$aFName['orig_ext']));
+		$img_file = $this->PHMedia->getFileName($type, $id, null, $aFName['orig_fname'].'.'.$aFName['orig_ext']);
+		$image->load($img_file);
 
 		if ($aSize) {
 			$image->resize($aSize['w'], $aSize['h']);
